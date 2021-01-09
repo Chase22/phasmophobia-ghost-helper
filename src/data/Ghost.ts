25933f7a934f86f4dc4fc0bc2119a1f118387c97
Chase22 <chase@mailbox.org>
2021-01-09 09:35:09 +0100

    FIX: Jinn has EMF_LEVEL_5 as evidence, not FREEZING_TEMPERATURE

--- src/data/Ghost.ts
+++ src/data/Ghost.ts
@@ -6,18 +6,18 @@ export interface Ghost {
 }
 
 const ghosts: Array<Ghost> = [
-    {name: "Demon", evidence: ["SPIRIT_BOX", "FREEZING_TEMPERATURE", "GHOST_WRITING"]},
     {name: "Banshee", evidence: ["FINGERPRINTS", "EMF_LEVEL_5", "FREEZING_TEMPERATURE"]},
-    {name: "Spirit", evidence: ["SPIRIT_BOX", "FINGERPRINTS", "GHOST_WRITING"]},
-    {name: "Wraith", evidence: ["SPIRIT_BOX", "FINGERPRINTS", "FREEZING_TEMPERATURE"]},
+    {name: "Demon", evidence: ["SPIRIT_BOX", "FREEZING_TEMPERATURE", "GHOST_WRITING"]},
+    {name: "Jinn", evidence: ["GHOST_ORB", "SPIRIT_BOX", "EMF_LEVEL_5"]},
+    {name: "Mare", evidence: ["GHOST_ORB", "SPIRIT_BOX", "FREEZING_TEMPERATURE"]},
+    {name: "Oni", evidence: ["SPIRIT_BOX", "EMF_LEVEL_5", "GHOST_WRITING"]},
     {name: "Phantom", evidence: ["GHOST_ORB", "EMF_LEVEL_5", "FREEZING_TEMPERATURE"]},
     {name: "Poltergeist", evidence: ["GHOST_ORB", "SPIRIT_BOX", "FINGERPRINTS"]},
-    {name: "Jinn", evidence: ["GHOST_ORB", "SPIRIT_BOX", "FREEZING_TEMPERATURE"]},
-    {name: "Mare", evidence: ["GHOST_ORB", "SPIRIT_BOX", "FREEZING_TEMPERATURE"]},
     {name: "Revenant", evidence: ["FINGERPRINTS", "EMF_LEVEL_5", "GHOST_WRITING"]},
     {name: "Shade", evidence: ["GHOST_ORB", "EMF_LEVEL_5", "GHOST_WRITING"]},
+    {name: "Spirit", evidence: ["SPIRIT_BOX", "FINGERPRINTS", "GHOST_WRITING"]},
+    {name: "Wraith", evidence: ["SPIRIT_BOX", "FINGERPRINTS", "FREEZING_TEMPERATURE"]},
     {name: "Yurei", evidence: ["GHOST_ORB", "FREEZING_TEMPERATURE", "GHOST_WRITING"]},
-    {name: "Oni", evidence: ["SPIRIT_BOX", "EMF_LEVEL_5", "GHOST_WRITING"]},
 ]
 
 export default ghosts
